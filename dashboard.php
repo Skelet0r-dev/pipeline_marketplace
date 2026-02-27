0db407da1b27fda143d831820d00a0511bb0e0a2
Gabriel Angelo <gabrielangelosardidomanuel@gmail.com>
2026-02-28 00:11:37 +0800

    finished login.php

--- dashboard.php
+++ dashboard.php
@@ -1,3 +1,57 @@
+<?php
+$serverName = ".\SQLEXPRESS";
+$connectionOptions = [
+    "Database" => "pipeline_db",
+    "Uid" => "", 
+    "PWD" => "",
+];
+$conn = sqlsrv_connect($serverName, $connectionOptions);
+$std_num = $_POST['stdnum'];
+$password = $_POST['password'];
+
+$sql = "SELECT *
+        FROM dbo.[USERS] 
+        WHERE STD_NUM = '$stdnum'";
+$result = sqlsrv_query($conn, $sql);
+$rowname = sqlsrv_fetch_array($result);
+if ($rowname == null) {
+    echo "<script>
+                alert('Student Number not found!');
+                window.history.back();
+              </script>";
+        exit;
+}
+
+$sqlpassword = "SELECT *
+                FROM dbo.[USERS] 
+                WHERE STD_NUM = '$stdnum' AND PASSWORD = '$password'";
+$resultpassword = sqlsrv_query($conn, $sqlpassword);
+$rowpassword = sqlsrv_fetch_array($resultpassword);
+if ($rowpassword == null) {
+    echo "<script>
+                alert('Wrong Password!');
+                window.history.back();
+              </script>";
+        exit;
+}
+$loginId = $rowpassword['USER_ID'];
+
+$sqlprofile = "SELECT *
+                FROM dbo.[USER_IMG] 
+                WHERE USER_ID = '$std_num'";
+$resultprofile = sqlsrv_query($conn, $sqlprofile);
+if ($resultprofile === false) {
+    die("PROFILE QUERY ERROR:<br>" . print_r(sqlsrv_errors(), true));
+}
+
+$rowprofile = sqlsrv_fetch_array($resultprofile);
+$file_path = $rowprofile['FILE_PATH'];
+$firstname = $rowpassword['FIRST_NAME'];
+
+?>
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -16,7 +70,8 @@
 
         <div class="d-flex align-items-center gap-1">
             <p class="field-label mb-0">Hello,</p>
-            <p class="fw-bold mb-0">Name of Customer</p>
+            <p class="fw-bold mb-0"><?php echo $firstname?></p>
+            <img src="<?php echo $file_path ?>" class="img-profile" alt="Profile Picture">
         </div>
 
     </div>
